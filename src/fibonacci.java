4a2ab992d686533581da9c52bdaf6c41685c8e8a
Bedirhan <albayrakbedirhan570@gmail.com>
2023-04-15 09:01:09 +0300

    Initial commit

---
+++ src/fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kaç elemanlı Fibonacci serisi hesaplanacak?: ");
+        int n = scanner.nextInt();
+
+        int a = 0, b = 1;
+        System.out.print(a + " " + b);
+
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
